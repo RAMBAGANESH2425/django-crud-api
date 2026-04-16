e4ef9c8088c1048b4ef301111d2cce3f2a3d5411
Ramba Ganesh <ammananna8513@gmail.com>
2026-04-16 16:17:33 +0530

    Django REST API project

---
+++ api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
