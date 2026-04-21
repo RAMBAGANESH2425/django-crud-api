70028db9ccd99d8c52c6cd015be548169d028751
Ramba Ganesh <ammananna8513@gmail.com>
2026-04-21 18:18:15 +0530

    Added user authentication and linked Student model

--- api/models.py
+++ api/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Student(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='students')
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    email = models.EmailField()
+    course = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-
-
-class Course(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
